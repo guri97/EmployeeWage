bea82601562f72cf8da557156c754d0c6d5996a6
guri97 <guri84096@gmail.com>
2022-06-08 17:17:32 +0530

    [gurpreet]Employeedailywage

---
+++ Employeewage/Employeewage/Employeepresent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeewage
+{
+    internal class DailyEmpWage
+    {
+        int Is_EmpPresent = 1;
+        int Wage_Per_Hour = 20;
+        int FullDay_Hour = 8;
+        int DailyWage = 0;
+
+        public void CheckDailyEmpWg()
+        {
+            Random CheckEmp = new Random();
+            int value = CheckEmp.Next(0, 2);
+            if (value == Is_EmpPresent)
+            {
+                DailyWage = FullDay_Hour * Wage_Per_Hour;
+
+                Console.WriteLine("Employee Present and DailyWage = {0}", DailyWage);
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- Employeewage/Employeewage/Program.cs
+++ Employeewage/Employeewage/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Program
+﻿namespace Employeewage
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage");
+            DailyEmpWage employeepresent = new DailyEmpWage();
+            employeepresent.CheckDailyEmpWg();
+
+
         }
     }
 }
