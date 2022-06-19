f43b8381347721213788d5a6588e2c44d3492d16
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-19 12:43:24 +0530

    [gurpreet] UC11_Interface

---
+++ UC11/Interface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{ 
+        public interface ICompute_Emp_Wage
+        {
+
+            public void AddCompany(String CompanyName, int EmpWagePerhour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHours_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
+            {
+
+            }
+        }
+}
