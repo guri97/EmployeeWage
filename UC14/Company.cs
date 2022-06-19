b400cf292915a80e7f3b70cc76fe3f61c107d480
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-19 13:31:28 +0530

    [gurpreet] UC14_CompanyQueries

---
+++ UC14/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage5
+{
+
+        class Company
+        {
+            public float EmpWagePerHour;
+            public int FullTime_WorkingHrs_PerDay;
+            public int PartTime_WorkingHours_PerDay;
+            public int MAX_WORKING_HRS;
+            public int MAX_WORKING_DAYS;
+            public string CompanyName;
+
+            public Company(String CompanyName, int EmpWagePerhour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHours_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
+            {
+                this.CompanyName = CompanyName;
+                this.EmpWagePerHour = EmpWagePerhour;
+                this.FullTime_WorkingHrs_PerDay = FullTime_WorkingHrs_PerDay;
+                this.PartTime_WorkingHours_PerDay = PartTime_WorkingHours_PerDay;
+                this.MAX_WORKING_HRS = MAX_WORKING_HRS;
+                this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+
+            }
+
+        }
+    
+}
+
